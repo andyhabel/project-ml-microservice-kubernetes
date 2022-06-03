34d150647596a7a23997fa7991b1d6da0f6b6fc0
Your Name <you@example.com>
2022-06-03 14:09:19 -0600

    updated for task 4

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,15 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=andyhabel/mlapp
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag mlapp $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:latest
+
